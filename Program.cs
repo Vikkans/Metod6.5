493966800c8ea225250317e1aaf5e247f768272e
Pingvu <nhlpool27@gmail.com>
2024-02-14 13:22:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace forratinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett ord");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("Skriv in ett till ord");
+            string ord2 = Console.ReadLine();
+
+            Console.WriteLine(Längst(ord1, ord2));
+
+            
+
+
+        }
+
+        static string Längst(string ord1, string ord2)
+        {
+            if (ord1.Length > ord2.Length)
+            {
+                return ord1;
+            }
+
+            if (ord1.Length < ord2.Length)
+            {
+                return ord2;
+            }
+            else
+            {
+                return ord1;
+            }
+
+        }
+          
+    }
+}
